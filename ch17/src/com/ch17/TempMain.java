0202704b0d65f6f2ca7dd3551b2b872592224e25
seungh0 <will.seungho@gmail.com>
2020-12-31 22:46:10 +0900

    feat(ch17): Processor로 데이터 변환하기

--- ch17/src/com/ch17/TempMain.java
+++ ch17/src/com/ch17/TempMain.java
@@ -6,10 +6,19 @@ public class TempMain {
 
 	public static void main(String[] args) {
 		getTemperatures("New York").subscribe(new TempSubscriber());
+		getCelsiusTemperatures("New York").subscribe(new TempSubscriber());
 	}
 
 	private static Flow.Publisher<TempInfo> getTemperatures(String town) {
 		return subscriber -> subscriber.onSubscribe(new TempSubscription(subscriber, town));
+	}
+
+	private static Flow.Publisher<TempInfo> getCelsiusTemperatures(String town) {
+		return subscriber -> {
+			TempProcessor processor = new TempProcessor();
+			processor.subscribe(subscriber);
+			processor.onSubscribe(new TempSubscription(processor, town));
+		};
 	}
 
 }
